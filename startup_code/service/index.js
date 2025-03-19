b830d8f922ab94d47ff0d9d84ecf31377be23f92
mikelj01 <mikelj01@byu.edu>
2025-03-19 17:52:02 -0600

    added account creation routing

--- startup_code/service/index.js
+++ startup_code/service/index.js
@@ -1,12 +1,45 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
+
+const authCookieName = 'token';
+
+
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+
+let users = [];
+let chairs = [];
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
 app.get('*', (_req, res) => {
+  console.log('Yeet');
   res.send({ msg: 'Loveseat service' });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'That Email is Already Associated with an account' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
 });
