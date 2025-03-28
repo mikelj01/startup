10167461b22499710c18eb17c900f969a7e5d9e9
mikelj01 <mikelj01@byu.edu>
2025-03-28 16:44:10 -0600

    adding the db communication stuff

---
+++ startup_code/service/database.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('simon');
+const userCollection = db.collection('user');
+const messageCollection = db.collection('message');
+
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+async function addmessage(message) {
+  return scoreCollection.insertOne(message);
+}
